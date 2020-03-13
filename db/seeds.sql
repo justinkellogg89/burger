171c70f5004f2d893ae45d777c25a27a18a08aef
Justin <jlkellogg18@outlook.com>
2020-03-12 21:00:25 -0700

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('burgera', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('burgerb', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('burgerc', 0);
